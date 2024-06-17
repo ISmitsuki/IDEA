5b9e37065164d65f39566898d19a1d4a4e7cfa78
ISmitsuki <120702448@qq.com>
2024-06-17 12:35:37 +0800

    update version

--- src/PI.java
+++ src/PI.java
@@ -1,19 +1,39 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class PI {
     public static void main(String[] args) {
-        int n=0;
+        //input - scanner
+        //input: n,int,number of iter
+        Scanner reader = new Scanner(System.in);
+        System.out.println("please enter the iter number: ");
+        int iterNum=reader.nextInt();
 
-        Random rand = new Random();
-        for (int i = 0; i < 10000; i++) {
-            double x = rand.nextDouble()*2-1;
-            double y = rand.nextDouble()*2-1;
-            if(x*x+y*y<=1){
-                n++;
+        //double PI=computePI(n)
+        double estiPI = computePI(iterNum);
+
+        //verify estimated PI ~= true PI = Math.PI
+        double diff = (estiPI-Math.PI);
+
+        //print things out: Estimated PI value, diff
+        System.out.println("Estimated PI = "+ estiPI + ", Diff = " + diff);
+    }
+
+    public static double computePI(int iterNum_){
+        int counter = 0;
+        double PI=0;
+        for (int i = 0; i < iterNum_; i++) {
+            //take random samples
+            double xPos = Math.random();
+            double yPos = Math.random();
+
+            //verify condition
+            if (Math.sqrt((xPos * xPos) + (yPos * yPos)) <= 1) {
+                counter++;
             }
         }
-        //P=pi/4...pi=4P
-        double pi=4*n/10000.0;
-        System.out.println(pi);
+        PI = 4.0*counter/iterNum_;
+        return PI;
     }
+
 }
